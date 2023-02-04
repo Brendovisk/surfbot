81e0b6c44bd7f2b718264d013f1c9e2f0db82085
Brendon <brendon.franco@vibryt.com.br>
2023-02-04 18:27:26 -0300

    refactor: toggleplan class

--- src/js/modules/togglePlan.js
+++ src/js/modules/togglePlan.js
@@ -6,33 +6,30 @@ export default class TogglePlan {
   }
 
   removeActiveClass() {
-    this.trigger.forEach((input, index) => {
-      input.parentElement.classList.remove(this.activeClass);
+    this.trigger.forEach((element, index) => {
+      element.parentElement.classList.remove(this.activeClass);
       this.target[index].classList.remove(this.activeClass);
     });
   }
 
-  addActiveClass() {
-    this.trigger.forEach((input, index) => {
-      if (input.checked) {
-        input.parentElement.classList.add(this.activeClass);
-        this.target[index].classList.add(this.activeClass);
-      }
-    });
+  addActiveClass(index) {
+    this.trigger[index].checked = true;
+    this.trigger[index].parentElement.classList.add(this.activeClass);
+    this.target[index].classList.add(this.activeClass);
   }
 
   togglePlanEventListeners() {
-    this.trigger.forEach((input) => {
-      input.addEventListener("click", () => {
+    this.trigger.forEach((element, index) => {
+      element.addEventListener("click", () => {
         this.removeActiveClass();
-        this.addActiveClass();
+        this.addActiveClass(index);
       });
     });
   }
 
   init() {
     if (this.trigger.length && this.target.length && this.activeClass) {
-      this.addActiveClass();
+      this.addActiveClass(0);
       this.togglePlanEventListeners();
     }
     return this;
